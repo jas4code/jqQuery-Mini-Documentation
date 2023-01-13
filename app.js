5ffa37dd2306c5800bbc5a29e79c1d63212b8e12
jas4code <jasabocejo@protonmail.com>
2023-01-13 23:32:53 +0800

    jQuery Mini Documentation

---
+++ app.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+    $(".hidden").hide()
+    $(".show").show()
+
+    // Click Button
+    $("#click").click(function() {
+        alert("Display 'alert' mo daw");
+        // $("#main .main-content .content").hide();
+    })
+
+    // Hide Button
+    $("#hide").click(function() {
+        $("#hide-sample").hide();
+    })
+
+    // Show Button
+    $("#show").click(function() {
+        $("#show-sample").show();
+    })
+
+    // Toggle Switch
+    $("#toggle").click(function() {
+        $("#toggle-sample").toggle();
+    })
+
+    // SlideDown Button
+    $("#slideDown").click(function() {
+        $("#slideDown-sample").slideDown("slow");
+    })
+
+    // SlideDown Button
+    $("#slideUp").click(function() {
+        $("#slideUp-sample").slideUp("slow");
+    })
+
+    // SlideToggle Switch 
+    $("#slideToggle").click(function() {
+        $("#slideToggle-sample").slideToggle();
+    })
+
+    // fadeIn Button 
+    $("#fadeIn").click(3000, function() {
+        $("#fadeIn-sample").fadeIn("slow");
+    })
+
+    // fadeOut Button 
+     $("#fadeOut").click(3000, function() {
+        $("#fadeOut-sample").fadeOut("slow");
+    })
+
+
+})
